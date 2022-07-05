58f9dd055338c43a403475e520523579ce88b317
InnaSolod <solodinna6@gmail.com>
2022-07-05 16:53:27 +0300

    Save-8

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,25 @@
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
+//   const bodyOverscroll = document.querySelector('body');
+
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//     menuBtnRef.classList.toggle('--is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//     mobileMenuRef.classList.toggle('--is-open');
+//     bodyOverscroll.classList.toggle('no-scroll');
+//   });
+// })();
+
+
+
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+     const bodyOverscroll = document.querySelector('body');
   
     menuBtnRef.addEventListener("click", () => {
       const expanded =
@@ -10,5 +29,6 @@
       menuBtnRef.setAttribute("aria-expanded", !expanded);
   
       mobileMenuRef.classList.toggle("is-open");
+      bodyOverscroll.classList.toggle('modal-open');
     });
   })();
